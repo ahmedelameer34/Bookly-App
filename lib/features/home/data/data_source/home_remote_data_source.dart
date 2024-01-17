728e7c8e21f50fdaccee5038541ecdd06edef981
Ahmed Badawy Mohamed El Amir <amirbadawy68@gmail.com>
2024-01-17 10:58:15 +0200

    -

--- lib/features/home/data/data_source/home_remote_data_source.dart
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -19,7 +19,7 @@ class HomeRemoteDataSourceImepl extends HomeRemoteDataSource {
   Future<List<BookEntity>> fetchBestSeller({int pageNum = 0}) async {
     var data = await apiServices.get(
         endPoint:
-            '?filtering=adventure&q=hacking&startIndex=${pageNum * 10}&maxResults=10');
+            '?filtering=bestseller&q=novels&startIndex=${pageNum * 10}&maxResults=10');
     List<BookEntity> books = getBookList(data);
     saveBooks(books, kBestBooksBox);
     return books;
@@ -46,7 +46,7 @@ class HomeRemoteDataSourceImepl extends HomeRemoteDataSource {
   Future<List<BookEntity>> fetchOlsoLike({int pageNum = 0}) async {
     var data = await apiServices.get(
         endPoint:
-            '?filtering=novels&q=adventure&startIndex=${pageNum * 10}&maxResults=10');
+            '?filtering=bestseller&q=novels&startIndex=${pageNum * 10}&maxResults=10');
     List<BookEntity> books = getBookList(data);
     saveBooks(books, kOlsoLikeBooksBox);
     return books;
